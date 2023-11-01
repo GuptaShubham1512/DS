d87a7ab64ff2cdeabce9426c1ffe637a6bf5ea1a
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2023-11-01 14:28:02 +0000

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,4 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int main
+//count number of  elements strictly greater than value x
+int main(){
+    vector<int> v(6);
+    for(int i=0;i<v.size();i++){
+        cin>>v[i];
+    }
+
+    cout<<"enter the value of x:";
+    int x;
+    cin>>x;
+
+    int count=0;
+    for(int i=0;i<v.size();i++){
+        if(v[i]>x){
+            count++;
+        }
+    }
+
+    cout<<count<<endl;
+}
