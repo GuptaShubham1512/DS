9cc6f46a46049f5faa5a9d3547ec07a22f6d3e10
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2023-11-02 17:48:20 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+//Targetsumm problem 
+int main()
+{
+    int array[] = {3,4,6,7,1};
+    int targetsum = 7;
+    int size =5;
+    int pairs=0;
+    
+    for(int i=0;i<size;i++){
+        for(int j=i+1;j<size;j++){
+            
+            if(array[i]+array[j]==targetsum){
+                pairs++;
+            }
+        }
+}
+cout<<pairs<<endl;
+}
