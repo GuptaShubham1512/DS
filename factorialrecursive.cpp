1726a0ac4b2ab1ccfa3d6599bdf16a22c790b4c3
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-03-27 18:28:01 +0530

    Add files via upload

--- factorialrecursive.cpp
+++ factorialrecursive.cpp
@@ -1,14 +1,29 @@
-#include<iostream>
-using namespace std; 
+//#include<iostream>
+//using namespace std; 
 
-int f(int n){
+//int f(int n){
     //base case
-    if(n==1) return 1;
-    return n*f(n-1);
+    //if(n==1) return 1;
+  //  return n*f(n-1);
+//}
+
+//int main(){
+    //int result=f(6);
+    //cout<<result;
+  //  return 0;
+//}
+//FIBONACCI NUMBER SERIES PROBLEM
+#include<iostream>
+using namespace std;
+
+int  fib(int n){
+    //base case
+    if(n==0 or n==1) return n;
+    return fib(n-1) + fib(n-2);
 }
 
 int main(){
-    int result=f(6);
+    int result = fib(5);
     cout<<result;
     return 0;
 }
