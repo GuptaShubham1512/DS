c0dd3d84128760a299325ce87742858b971e895b
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-04-08 01:22:25 +0530

    Add files via upload

---
+++ Maximum Array Rec.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+//MAXIMUM ARRAY PROBLEM USING RECURSION
+
+//int f( int *arr,int idx, int n){
+    //base case
+  //  if(idx==n-1){
+        //only one element left
+    //    return arr[idx];
+    //}
+    //return max(arr[idx], f(arr,idx+1,n));
+//}
+
+//int main(){
+  //  int arr[] = {3,2,10,8,6};
+    //int n=5;
+    //cout<<f (arr,0,n);
+    //return 0;
+    
+//} 
+
+//Sum of array using recursion
+
+int f( int *arr,int idx, int n){
+    //base case
+    if(idx==n-1){
+        //only one element left
+        return arr[idx];
+    }
+    return arr[idx]+f(arr,idx+1,n);
+}
+
+int main(){
+    int arr[] = {3,2,10,8,6};
+    int n=5;
+    cout<<f (arr,0,n);
+    return 0;
+    
+} 
