835b032b2ceb94c198aa55d62113e2b7bf71948d
GuptaShubham1512 <148928391+GuptaShubham1512@users.noreply.github.com>
2024-04-08 23:06:37 +0530

    Add files via upload

---
+++ String Rec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+string f(string&s ,int idx ,int n){
+    //base case
+    if(idx==n) return " ";
+    string curr=" ";
+    curr += s[idx];
+    return((s[idx]=='a')?" ":curr) + f(s, idx+1,n);
+}
+int main(){
+string s="abcgan";
+int n=6;
+cout<<f(s,0,n);
+return 0;
+}
